2ea45bbf0d28c9a283a46ca8bc072abafb594cc5
electron65 <57064558+electron65@users.noreply.github.com>
2022-02-13 02:13:25 +0900

    Add files via upload

---
+++ testbench/not_test.v
@@ -0,0 +1,22 @@
+`timescale 1ns/1ns
+
+module not_gate_test; //module name for testbench
+reg [2:0] A; // register for input
+wire X; // wire for output
+integer i;
+
+//not_gate not_gate_test(A, X);
+not_gate not_gate_test(.a(A[0]), .x(X));
+
+initial
+begin
+	A = 3'bZ;
+
+	for(i = 0; i < 8; i = i + 1)
+
+		#20 A = i;
+end
+
+//assign #20 A = ~A;
+
+endmodule
